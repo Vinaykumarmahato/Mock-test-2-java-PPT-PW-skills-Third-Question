13aac6b97a638e6cc293a21908516bfd2da269ef
ADV Indian coder <116486037+ADVindiancoder@users.noreply.github.com>
2023-07-12 08:59:22 -0700

    Mock-test-2-java-PPT-PW-skills-Third-Question

---
+++ que3/que3.java
@@ -0,0 +1,43 @@
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
+
+public class que3 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("John");
+        person.setAge(30);
+        person.setAddress("123 Main St");
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Address: " + person.getAddress());
+    }
+}
